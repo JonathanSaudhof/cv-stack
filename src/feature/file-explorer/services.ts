c9f85b0d40af015559a21a17da5ffaff3a4caba2
JonathanSaudhof <jonathan.saudhof@gmail.com>
2024-12-24 12:17:07 +0100

    chore: adds shadcn ui

--- src/feature/file-explorer/services.ts
+++ src/feature/file-explorer/services.ts
@@ -4,7 +4,7 @@ import { env } from "@/env";
 import { auth } from "@/server/auth";
 import { google } from "googleapis";
 
-async function getGoogleOauth() {
+async function getAuthenticatedDrive() {
   const session = await auth();
   const clientId = env.GOOGLE_CLIENT_ID;
   const clientSecret = env.GOOGLE_CLIENT_SECRET;
@@ -20,12 +20,11 @@ async function getGoogleOauth() {
     refresh_token: refreshToken,
   });
 
-  return oauth;
+  return google.drive({ auth: oauth, version: "v3" });
 }
 
 export async function getAllFilesInFolder(folderId?: string) {
-  const oauth = await getGoogleOauth();
-  const drive = google.drive({ auth: oauth, version: "v3" });
+  const drive = await getAuthenticatedDrive();
 
   try {
     const res = await drive.files.list({
@@ -37,5 +36,84 @@ export async function getAllFilesInFolder(folderId?: string) {
     return res.data.files;
   } catch (error) {
     throw error;
+  }
+}
+
+export async function downloadFile(fileId: string) {
+  const drive = await getAuthenticatedDrive();
+
+  try {
+    const res = await drive.files.get(
+      {
+        fileId,
+        alt: "media",
+      },
+      { responseType: "stream" },
+    );
+
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+type Config = {
+  folderId: string;
+  defaultTemplateDocId: string;
+};
+
+const CONFIG_FILE_NAME = "config.json";
+
+export async function getConfigFile() {
+  const drive = await getAuthenticatedDrive();
+
+  try {
+    const config = await drive.files.list({
+      q: `name = '${CONFIG_FILE_NAME}'`,
+      fields: "nextPageToken, files(id, name)",
+      spaces: "appDataFolder",
+    });
+
+    return config.data.files ? config.data.files[0] : null;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function createConfigFile() {
+  const drive = await getAuthenticatedDrive();
+
+  try {
+    const res = await drive.files.create({
+      requestBody: {
+        name: CONFIG_FILE_NAME,
+        mimeType: "application/json",
+        parents: ["appDataFolder"],
+      },
+      media: {
+        mimeType: "application/json",
+        body: JSON.stringify({
+          folderId: "root",
+          defaultTemplateDocId: "",
+        }),
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getFileById(fileId: string) {
+  const drive = await getAuthenticatedDrive();
+
+  try {
+    return await drive.files.get({
+      fileId,
+      fields: "id, name, mimeType",
+    });
+  } catch (error) {
+    console.error(error);
   }
 }
