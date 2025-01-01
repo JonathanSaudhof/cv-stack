704cbe27814deb79a62a2bed20fab613942b76f6
JonathanSaudhof <jonathan.saudhof@gmail.com>
2025-01-01 19:15:03 +0100

    chore: adds skeleton and suspense boundary for application list

--- src/feature/application/components/application-list.tsx
+++ src/feature/application/components/application-list.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/trpc/server";
 import { Archive, File, Link, SquareArrowOutUpRight } from "lucide-react";
-import { Application } from "../schema";
+import { type Application } from "../schema";
 
 export default async function ApplicationsList({
   folderId,
@@ -24,10 +25,7 @@ export default async function ApplicationsList({
 
 function ApplicationItem(application: Readonly<Application>) {
   return (
-    <li
-      key={application.folderId}
-      className="flex items-center gap-4 rounded border p-4"
-    >
+    <li className="flex items-center gap-4 rounded border p-4">
       <div className="flex flex-1 gap-8">
         <div className="flex w-1/5 flex-col gap-1">
           <p className="text-xs text-gray-300">Company</p>
@@ -63,6 +61,56 @@ function ApplicationItem(application: Readonly<Application>) {
           <span className="sr-only">CV</span>
         </Button>
         <Button variant="ghost">
+          <Archive />
+          <span className="sr-only">Archive</span>
+        </Button>
+      </div>
+    </li>
+  );
+}
+
+export function ApplicationListSkeleton() {
+  return (
+    <section className="flex flex-col gap-4 p-8">
+      <h2 className="text-2xl font-semibold">Applications</h2>
+      <ul className="flex flex-col gap-2">
+        <ApplicationItemSkeleton />
+        <ApplicationItemSkeleton />
+        <ApplicationItemSkeleton />
+      </ul>
+    </section>
+  );
+}
+
+function ApplicationItemSkeleton() {
+  return (
+    <li className="flex items-center gap-4 rounded border p-4">
+      <div className="flex flex-1 gap-8">
+        <div className="flex w-1/5 flex-col gap-1">
+          <p className="text-xs text-gray-300">Company</p>
+          <Skeleton className="h-[16px] w-[100px] rounded-full" />
+        </div>
+        <div className="flex flex-1 flex-col gap-1">
+          <p className="text-xs text-gray-300">Job Title</p>
+          <Skeleton className="h-[16px] w-[200px] rounded-full" />
+        </div>
+        <div className="flex w-1/5 flex-col gap-1">
+          <p className="text-xs text-gray-300">Status</p>
+          <Skeleton className="h-[16px] w-[100px] rounded-full" />
+        </div>
+      </div>
+      <div className="flex w-1/6 justify-end border-l pl-4">
+        <Button variant="ghost" title="Job Description" disabled>
+          <Link />
+        </Button>
+        <Button variant="ghost" title="Document's Folder" disabled>
+          <SquareArrowOutUpRight />
+        </Button>
+        <Button variant="ghost" disabled>
+          <File />
+          <span className="sr-only">CV</span>
+        </Button>
+        <Button variant="ghost" disabled>
           <Archive />
           <span className="sr-only">Archive</span>
         </Button>
